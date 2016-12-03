6179155e76ddbaf37f1ac1731df9b43e988f3e45
samonras <57160440@go.buu.ac.th>
2016-12-03 14:49:53 +0700

    Add a Static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def kingsong1
+  end
+
+  def kingsong2
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get kingsong1" do
+    get static_pages_kingsong1_url
+    assert_response :success
+  end
+
+  test "should get kingsong2" do
+    get static_pages_kingsong2_url
+    assert_response :success
+  end
+
+end
